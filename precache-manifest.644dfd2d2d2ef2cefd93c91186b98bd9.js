7171727164ddb7806738d3965360f78576f481fd
douglas-he <doughrqe@gmail.com>
2020-05-09 19:02:02 -0300

    Updates

---
+++ precache-manifest.644dfd2d2d2ef2cefd93c91186b98bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbf34caf2e546ba8b04f7990a42e8e1",
+    "url": "/starwars-database-redux/index.html"
+  },
+  {
+    "revision": "77d671adf8c43c1d614c",
+    "url": "/starwars-database-redux/static/css/main.1b6d6689.chunk.css"
+  },
+  {
+    "revision": "b7c14aa7e0c3f923fd3b",
+    "url": "/starwars-database-redux/static/js/2.84f42ed2.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/starwars-database-redux/static/js/2.84f42ed2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77d671adf8c43c1d614c",
+    "url": "/starwars-database-redux/static/js/main.d3f81fbe.chunk.js"
+  },
+  {
+    "revision": "b14b30caa304dc097b9c",
+    "url": "/starwars-database-redux/static/js/runtime-main.6f492b0c.js"
+  }
+]);
